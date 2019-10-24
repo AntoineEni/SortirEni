c2af1e9ac582ddc228b3b2afe7be5c4e6f53857b
clem180 <clemdevers@hotmail.fr>
2019-10-24 14:57:38 +0200

    save front

--- src/Form/EventFiltreType.php
+++ src/Form/EventFiltreType.php
@@ -17,47 +17,47 @@ class EventFiltreType extends AbstractType
         $builder
             ->add('site', EntityType::class, array(
                 'class' => 'App\Entity\Site',
-                'label' => 'Site :',
+                'label' => 'Site',
                 'choice_label' => 'name',
                 'placeholder' => 'Sélectionnez un lieu',
                 'attr'=>['id'=>'site','class'=>'form-control'],
-                'label_attr'=>['class'=>'col-sm-3 col-form-label']
+                'label_attr'=>['class'=>'input-group-text']
             ))
             ->add('dateMin',TextType::class,array(
                 'attr'=>['id'=>'min','class'=>'form-control js-datepicker'],
                 'mapped'=>false,
-                'label' => 'Date min :',
-                'label_attr'=>['class'=>'col-sm-3 col-form-label']
+                'label' => 'To',
+                'label_attr'=>['class'=>'input-group-text']
             ))
             ->add('dateMax',TextType::class,array(
                 'attr'=>['id'=>'max','class'=>'form-control js-datepicker'],
                 'mapped'=>false,
-                'label' => 'Date max :',
-                'label_attr'=>['class'=>'col-sm-3 col-form-label']
+                'label' => 'Date',
+                'label_attr'=>['class'=>'input-group-text']
             ))
             ->add('organisateur',CheckboxType::class,array(
                 'attr'=>['id'=>'organisateur','class'=>''],
                 'mapped'=>false,
                 'label' => 'Sortie dont je suis l\'organisateur/trice :',
-                'label_attr'=>['class'=>'col-sm-8 ']
+                'label_attr'=>['class'=>'form-control']
             ))
             ->add('inscrit',CheckboxType::class,array(
                 'attr'=>['id'=>'inscrit','class'=>''],
                 'mapped'=>false,
                 'label' => 'Sortie auxqelles je suis inscrit/e :',
-                'label_attr'=>['class'=>'col-sm-8 ']
+                'label_attr'=>['class'=>'form-control']
             ))
             ->add('nInscrit',CheckboxType::class,array(
                 'attr'=>['id'=>'Ninscrit','class'=>''],
                 'mapped'=>false,
                 'label' => 'Sortie auxqelles je ne suis pas inscrit/e :',
-                'label_attr'=>['class'=>'col-sm-8 ']
+                'label_attr'=>['class'=>'form-control']
             ))
             ->add('finie',CheckboxType::class,array(
                 'attr'=>['id'=>'passer','class'=>' '],
                 'mapped'=>false,
                 'label' => 'Sortie passées :',
-                'label_attr'=>['class'=>'col-sm-8 col-form-label']
+                'label_attr'=>['class'=>'form-control']
             ))
         ;
     }
