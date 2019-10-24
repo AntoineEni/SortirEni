439208f2c210b9058d378e08a10cde8690281086
AntoineEni <antoine.lepage2018@campus-eni.fr>
2019-10-24 11:06:31 +0200

    Remove useless commented functions

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -38,33 +38,4 @@ class EventRepository extends ServiceEntityRepository
             ->getArrayResult()
             ;
     }
-
-    // /**
-    //  * @return Event[] Returns an array of Event objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('e')
-            ->andWhere('e.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('e.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Event
-    {
-        return $this->createQueryBuilder('e')
-            ->andWhere('e.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
